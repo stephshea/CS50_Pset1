2803c46a52f55eba01ac89c965ca8953cdad189c
Stephanie Shea <stephcshea@gmail.com>
2018-04-02 20:56:14 +0000

    mario working

--- mario/less/mario4.c
+++ mario/less/mario4.c
@@ -3,29 +3,25 @@
 int main(void)
 {
     int n;
+
+    //getting height of pyramid
     eprintf("about to prompt for number\n");
+
     do
     {
-        n = get_int("Enter a positive number: ");
+        n = get_int("Enter height: ");
     }
-    while(n <= 0 || n >24);
-
-    //print out this many rows
+    while (n <= 0 || n > 24);
+    printf("\n");
     for (int i = 0; i < n; i++)
-  {
+    {
+        for (int j = 0; j <= n - i - 1; j++)
+            printf(" ");
     //print out this many columns
-      for (int j = 0; j < n; j++)
-      {
-       if( j< n){
-       printf(" ");
-      }
-      else {
-
-       printf("#");
-}
-printf("\n");
-  }
-}
+       for (int j = 0; j < i+2; j++)
+    printf("#");
+    printf("\n");
+    }
 }
 
 //debug50 ./mario4
