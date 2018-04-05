cf1e66e6c24967bfabed9e5871c97350da92fed6
Stephanie Shea <stephcshea@gmail.com>
2018-04-05 11:31:05 +0000

    completed Mario less comfortable

--- mario/less/mario.c
+++ mario/less/mario.c
@@ -2,17 +2,42 @@
 #include <stdio.h>
 int main(void)
 {
-    int n;
+    int r, h, s, n;
+    //getting height of pyramid
+    // eprintf("about to prompt for number\n");
     do
     {
-        n = get_int("Enter a positive number: ");
+        n = get_int("Enter height: ");
     }
+    while (n < 0 || n > 23);
 
-    while(n <= 0);
-
-    for (int i = 0; i < n; i++)
-  {
-      printf("#\n");
-  }
-
+    //sets up rows
+    for (r = 1; r <= n; r++)
+    {
+        //prints spaces
+        for (s = 1; s < n - r + 1; s++)
+        {
+            printf(" ");
+        }
+        //prints #s
+        for (h = 0; h < n - s + 2; h++)
+        {
+            printf("#");
+        }
+        //creates new row
+        printf("\n");
+    }
+    return 0;
 }
+//     printf("\n");
+//     for (int i = 0; i < n; i++)
+//     {
+//      for (int j = 0; j <= n - i - 1; j++)
+//             printf(" ");
+//     //print out this many columns
+//      for (int j = 0; j < i + 2; j++)
+//             printf("#");
+//         printf("\n");
+//     }
+// }
+//debug50 ./mario4
