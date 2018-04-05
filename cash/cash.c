c8ca3a548232ef29ee87b41a1c5270e94e4dd09a
Stephanie Shea <stephcshea@gmail.com>
2018-04-05 12:19:16 +0000

    organized mario solutions in new folders

---
+++ cash/cash.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+
+    int fib(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fib(n-1) + fib(n-2);
+}
+]
