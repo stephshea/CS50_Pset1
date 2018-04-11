149119f9a1c7198d782fae7e313b19ff39ce4a01
Stephanie Shea <stephcshea@gmail.com>
2018-04-11 11:07:21 +0000

    refactored code, refined comments

--- cash/cash.c
+++ cash/cash.c
@@ -1,19 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
-float cash = 0.00;
-int cents;
-int count = 0;
+float cash = 0.00; //use float initially when get amount
+int cents; //variable for conversion of cash float to int
+int count = 0; //track number of coins needed
 int main(void)
+
 {
     do
     {
-        //use float initially to get amount
+        //promt user for amount
         cash = get_float("Enter cash: ");
         printf("cash %.2f\n", cash);
     }
+
     while (cash < 0);
-    //convcerts cash float to an int
+
+    //multiples cash float by 100 to convert to cents and converts cash float to an int
     cents = round(cash * 100.00);
     {
     //interates thru each coin value from highest to lowest
@@ -22,23 +25,23 @@ int main(void)
             //subtract 25 from cents until less than 25 remains
             cents = cents - 25;
             //add 1 to count for each 25 subtraction
-            count = count + 1;
+            count ++;
         }
         //repeats logic for 25 for each lesser increment
-        while (cents < 25 && cents >= 10)
+        while (cents >= 10)
         {
             cents = cents - 10;
-            count = count + 1;
+            count ++;
         }
-        while (cents < 10 && cents >= 5)
+        while (cents >= 5)
         {
             cents = cents - 5;
-            count = count + 1;
+            count ++;
         }
-        while (cents < 5 && cents >= 1)
+        while (cents >= 1)
         {
             cents = cents - 1;
-            count = count + 1;
+            count ++;
         }
     }
     //returns least amount of coins needed
