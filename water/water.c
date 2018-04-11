e87edff2af6309f1df2b0b88e6d60258309f5f1b
Stephanie Shea <stephcshea@gmail.com>
2018-04-11 11:17:37 +0000

    slight refactoring

--- water/water.c
+++ water/water.c
@@ -1,17 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 int main(void)
+
 {
-    int minutes;
+    int minutes; //stores input
     do
     {
         //prompt user for minutes
         minutes = get_int("Minutes: ");
     }
+
     //make sure minutes is a postive integer
     while (minutes < 0);
+
     //calculate number of bottles based on minutes
-    int bottles = (minutes * 192) / 16;
+    int bottles = (minutes * 12); // full equation (minutes * 192) / 16;
+
     //print calculated bottles
     printf("Bottles: %i\n", bottles);
 }
